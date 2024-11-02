bb9cd9be0179810cd91e9c5800baabcbe16def61
root <root@moritz>
2024-11-02 18:38:55 +0100

    MENU

--- livingCellsInPygame/life.py
+++ livingCellsInPygame/life.py
@@ -3,11 +3,12 @@ import random
 import sys
 import pygame
 
-#(0,0) (1,0) (2,0) ...
-#(0,1) (1,1) (2,1) ...
-#(0,2) (1,2) (2,2) ...
-#...
+# Letzte Bearbeitung: 02.11.24
 
+# SPAGHETTI CODE
+
+# TODO: SETTINGS MENU
+# IDEE: Predator können satt werden und die grünen zellen nicht mehr essen
 
 pygame.init()
 
@@ -15,6 +16,9 @@ pygame.init()
 pygame.font.init()
 font = pygame.font.SysFont("Open Sans",20)
 bigFont = pygame.font.SysFont("Open Sans",100)
+smallFont = pygame.font.SysFont("Open Sans",50)
+normalFont = pygame.font.SysFont("Open Sans",25)
+
 text_farbe = (255,255,255)
 
 screenWidth = 800
@@ -23,19 +27,26 @@ screen = pygame.display.set_mode((screenWidth,screenHeight))
 caption = pygame.display.set_caption("Game Of Life")
 clock = pygame.time.Clock()
 
+if screenWidth == 800 and screenHeight == 610:
+    rows = 50
+    cols = 50
+else:
+    rows = 200
+    cols = 200
+
 bodySize = 18
-rows = 50
-cols = 50
 cell_size = 18
 bodyColor = (124,252,0)
 max_cells = 100
 multiplier = 1
 delCellRed = []
 num_cells =  0
-numRedCells = 1
+numRedCells = 1 # Fix this <-- Program crashed wenn auf 0
 killOnes = False
 predatorNear = False
-speed = 1
+
+speedGreen = 1
+speedRed = 1
 
 # Warscheinlichkeiten standard werte
 rightTurn = 0.97
@@ -45,12 +56,14 @@ downTurn = 0.97
 
 rightTurnRed = 0.97
 leftTurnRed = 0.97
+upTurnRed = 0.97
+downTurnRed = 0.97
 
 cells = [[random.randint(0, cols - 1),random.randint(0,rows - 1)] for _ in range(num_cells)]
 redCells = [[random.randint(0,cols - 1),random.randint(0,rows - 1)] for _ in range(numRedCells)]
 
-print(num_cells)
-print(numRedCells)
+#print(num_cells)
+#print(numRedCells)
 
 def spawnGrid(screen):
     global x,y
@@ -96,8 +109,40 @@ def spawnCellRed():
     
     # GÜLTIGEN BEREICH SPAWNEN
     if 0 <= col < cols and 0 <= row < rows:
-        redCells.append([row - 1, col - 1])
+        redCells.append([row , col ])
         numRedCells += 1
+
+def infoScreen():
+    runner = True
+    while runner:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                runner = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit(0)
+                if event.key == pygame.K_ESCAPE:
+                    runner = False
+        screen.fill("black")
+        text = "This Game simulates small pixel living in a small grid."
+        text_2 = "You can spawn cells with your mouse buttons."
+        text_3 = "You can also respawn the cells in a random position with <SPACE>"
+
+        informationHeader = bigFont.render("Game Of Life",False,"white")
+        infoText = normalFont.render(text,False,"white")
+        infoText_2 = normalFont.render(text_2,False,"white")
+        infoText_3 = normalFont.render(text_3,False,"white")
+
+        infoText_4 = normalFont.render("READ MORE ON MY GITHUB: @Moritz344",False,"red")
+        
+        screen.blit(informationHeader,(100,0))
+        screen.blit(infoText,(5,200))
+        screen.blit(infoText_2,(5,250))
+        screen.blit(infoText_3,(5,300))
+        screen.blit(infoText_4,(10,400))
+
+        pygame.display.update()
+        clock.tick(60)
 
 def pauseScreen(width,height,font):
     running = True
@@ -109,10 +154,39 @@ def pauseScreen(width,height,font):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if respawnButtonBox.collidepoint(event.pos):
+                    running = False
+
+                if infoTextBox.collidepoint(event.pos):
+                    infoScreen()
+                    running = False
+
+        screen.fill("black")       
+        mouse = pygame.mouse.get_pos()
 
         pauseText = font.render("PAUSED",False,(255,255,255))
+        respawnButtonBox = pygame.draw.rect(screen,"black",(width // 2 - 200,height // 2 - 5 ,220,70))
 
-        screen.blit(pauseText,(width // 2 - 200 ,height // 2 - 90 ))
+        infoTextBox = pygame.draw.rect(screen,"black",(width // 2 - 200,height // 2 + 80,220,70))
+
+        if respawnButtonBox.collidepoint(mouse):
+            respawnButtonText = smallFont.render("Continue",False,"green") 
+        else:
+            respawnButtonText = smallFont.render("Continue",False,"white") 
+        
+        if infoTextBox.collidepoint(mouse):
+            infoText = smallFont.render("Info",False,"green")
+        else:
+            infoText = smallFont.render("Info",False,"white")
+
+
+
+        
+
+        screen.blit(respawnButtonText,(width // 2 - 200, height // 2 - 5))
+        screen.blit(pauseText,(width // 2 - 200 ,height // 2 - 150 ))
+        screen.blit(infoText,(width // 2 - 200 ,height // 2 + 80 ))
 
 
 
@@ -131,9 +205,8 @@ while run :
             if event.key == pygame.K_SPACE:
                 cells = [[random.randint(0, cols - 1),random.randint(0,rows - 1)] for _ in range(num_cells)]
                 redCells = [[random.randint(0,cols - 1),random.randint(0,rows - 1)] for _ in range(numRedCells)]
-                
+
                 print("random seed ")
-                #print("spawned food at random position.")
             if event.key == pygame.K_ESCAPE:
                 pauseScreen(screenWidth,screenHeight,bigFont)
 
@@ -155,24 +228,32 @@ while run :
 
         col,row = cells[i]
 
-        if direction == "RIGHT" and random.random() > rightTurn and 0 <= col :
-            col += speed
-            #print(f"moved to the right at {col}")
-        elif direction == "LEFT" and col > 0 and random.random() > leftTurn and col < cols:
-            col -= speed
-            #print(f"moved to the left at {col}")
+        new_col,new_row = col,row
 
-        elif direction == "UP" and row > 0 and random.random() > upTurn and 0 <= row:
-            row -= speed
-            #print(f"moved to up at {row}")
-        elif direction == "DOWN" and row < rows -1 and random.random() > downTurn and row < rows - 1:
-            row += speed
-            #print(f"moved down at {row}")
+        if direction == "RIGHT" and random.random() > rightTurn :
+            if col < cols - 1:
+                col += speedGreen
+            else:
+                direction = "LEFT"
+
+            #print(f"moved to the right at {col}")
+        elif direction == "LEFT" and random.random() > leftTurn :
+            if col > 0:
+                col -= speedGreen
+            else:
+                direction = "RIGHT"
+
+        elif direction == "UP" and random.random() > upTurn :
+            if row > 0:
+                row -= speedGreen
+            else:
+                direction = "DOWN"
+
+        elif direction == "DOWN" and random.random() > downTurn :
+            if row < rows - 1:
+                row += speedGreen
     
         # Update die Position der aktuellen Zelle
-
-            
-        #print("X",col,"Y:",row)
         cells[i] = [col,row]
 
 
@@ -182,15 +263,33 @@ while run :
         col2,row2 = redCells[i]
 
         if direction2 == "RIGHT" and  0 <= col2:
-            col2 += 1
+            if col2 < cols - 1:
+                col2 += speedRed
+            else:
+                direction = "LEFT"
+
         elif direction2 == "LEFT" and col2 > 0 and col2 < cols:
-            col2 -= 1
+            if col2 > 0:
+                col2 -= speedRed
+            else:
+                direction = "RIGHT"
+
         elif direction2 == "UP" and row2 > 0 and 0 <= row2:
-            row2 -= 1
+            if row2 > 0:
+                row2 -= speedRed
+            else:
+                direction = "DOWN"
+
         elif direction2 == "DOWN" and row2 < rows -1 and row2 < rows :
-            row2 += 1
+            if row2 < rows - 1:
+                row2 += speedRed
+            else:
+                direction = "UP"
         
+
         redCells[i] = col2,row2
+
+        #print(col2,row2)
 
 
     spawnGrid(screen)
@@ -230,7 +329,8 @@ while run :
                             col = g_col
                             break
                     except Exception as e:
-                        print(f"Program crashed: {e}")
+                        print(f"Program Error: Platziere die Rote Zelle nicht auf der Grünen! {e}")
+                        sys.exit(0)
 
             # Überbevölkerung
             elif num_cells >= 250:
