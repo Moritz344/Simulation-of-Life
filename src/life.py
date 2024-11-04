3c4c0eb76ede0a8950f4ec333bc79a38645e638a
root <root@moritz>
2024-11-04 21:28:35 +0100

    color remake/Namtags

--- src/life.py
+++ src/life.py
@@ -2,30 +2,27 @@ import time
 import random
 import sys
 import pygame
-
-# Letzte Bearbeitung: 02.11.24
+import customtkinter as ctk
 
 # SPAGHETTI CODE
 
-<<<<<<< HEAD
 # TODO: FORTPFLANZUNG
 
-=======
-# IDEE: Predator können satt werden und die grünen zellen nicht mehr essen
-# IDEE: Grüne Zellen können orangene Zellen herstellen die Predator töten
-# IDEE: Wurm zellen?
->>>>>>> 5d349904dda16cd71f09abed00f8bda5d34e1191
+# IDEE: ALTERUNG
+# IDEE: /
 
 pygame.init()
 
 # FONT
 pygame.font.init()
-font = pygame.font.SysFont("Open Sans",20)
-bigFont = pygame.font.SysFont("Open Sans",100)
-smallFont = pygame.font.SysFont("Open Sans",50)
-normalFont = pygame.font.SysFont("Open Sans",25)
+fontSize = 20
+font = pygame.font.Font("MinecraftRegular.otf",fontSize)
+bigFont = pygame.font.Font("MinecraftRegular.otf",100)
+smallFont = pygame.font.Font("MinecraftRegular.otf",50)
+normalFont = pygame.font.Font("MinecraftRegular.otf",25)
 
 text_farbe = (255,255,255)
+
 
 screenWidth = 800
 screenHeight = 610
@@ -37,7 +34,7 @@ wormSizex = 20
 wormSizey = 20
 
 greenCellTimer = pygame.time.get_ticks()
-timerDurationGreen = 1000
+timerDurationGreen = 10000
 
 
 if screenWidth == 800 and screenHeight == 610:
@@ -49,18 +46,21 @@ else:
 
 bodySize = 18
 cell_size = 18
-bodyColor = (124,252,0)
 max_cells = 100
 multiplier = 1
-delCellRed = []
 num_cells =  1
 numRedCells = 1 
 numBlueCells = 0
 killOnes = False
-
+age = 0
+nameTagColor = "white"
+nameTagVisible = True
 
 speedGreen = 1
 speedRed = 1
+
+cellColor2= (205,83,103)
+cellColor = (125,205,133)
 
 
 # Warscheinlichkeiten standard werte
@@ -77,8 +77,7 @@ downTurnRed = 0.97
 cells = [[random.randint(0, cols - 1),random.randint(0,rows - 1)] for _ in range(num_cells)]
 redCells = [[random.randint(0,cols - 1),random.randint(0,rows - 1)] for _ in range(numRedCells)]
 blueCells = [[random.randint(0,cols - 1),random.randint(0,rows - 1)] for _ in range(numBlueCells)]
-#print(num_cells)
-#print(numRedCells)
+
 
 def spawnGrid(screen):
     global x,y
@@ -88,7 +87,7 @@ def spawnGrid(screen):
             x = breite * cell_size
             y = höhe  * cell_size
 
-            pygame.draw.rect(screen,"black",(x,y,cell_size,cell_size),1)
+            pygame.draw.rect(screen,(30,32,25),(x,y,cell_size,cell_size),1)
 
 
 
@@ -150,7 +149,7 @@ def infoScreen():
         text_2 = "You can spawn cells with your mouse buttons."
         text_3 = "You can also respawn the cells in a random position with <SPACE>"
 
-        informationHeader = bigFont.render("Game Of Life",False,"white")
+        informationHeader = bigFont.render("Game Of Life",True,"white")
         infoText = normalFont.render(text,False,"white")
         infoText_2 = normalFont.render(text_2,False,"white")
         infoText_3 = normalFont.render(text_3,False,"white")
@@ -167,6 +166,7 @@ def infoScreen():
         clock.tick(60)
 
 def pauseScreen(width,height,font):
+    global nameTagColor,nameTagVisible,nameTagSurface
     running = True
     while running:
         for event in pygame.event.get():
@@ -188,36 +188,53 @@ def pauseScreen(width,height,font):
                     sys.exit(0) # exit the whole program
                     #running = False # exit PAUSED tab
 
-        screen.fill("black")       
+                # Aus und Einschalten von Nametags
+                if nameTagBox.collidepoint(event.pos):
+                    nameTagVisible = not nameTagVisible
+
+                
+               
+                
+
+
+        screen.fill((30,32,25))       
         mouse = pygame.mouse.get_pos()
         
-        quitTextBox = pygame.draw.rect(screen,"black",(width // 2 - 200 ,height // 2 + 160,220,70))
+        quitTextBox = pygame.draw.rect(screen,(30,32,25),(width // 2 - 200 ,height // 2 + 160,220,70))
 
-        pauseText = font.render("PAUSED",False,(255,255,255))
-        respawnButtonBox = pygame.draw.rect(screen,"black",(width // 2 - 200,height // 2 - 5 ,220,70))
-        infoTextBox = pygame.draw.rect(screen,"black",(width // 2 - 200,height // 2 + 80,220,70))
+        pauseText = font.render("PAUSED",True,(88,123,127))
+        respawnButtonBox = pygame.draw.rect(screen,(30,32,25),(width // 2 - 200,height // 2 - 5 ,220,70))
+        infoTextBox = pygame.draw.rect(screen,(30,32,25),(width // 2 - 200,height // 2 + 80,220,70))
+        visibleText = "NAMETAG ON"
+        nameTagBox = pygame.draw.rect(screen,(30,32,25),(0,0,220,50))
 
         if respawnButtonBox.collidepoint(mouse):
-            respawnButtonText = smallFont.render("Continue",False,"green") 
+            respawnButtonText = smallFont.render("Continue",True,"green") 
         else:
-            respawnButtonText = smallFont.render("Continue",False,"white") 
+            respawnButtonText = smallFont.render("Continue",True,(141,171,127)) 
         
         if infoTextBox.collidepoint(mouse):
-            infoText = smallFont.render("Info",False,"green")
+            infoText = smallFont.render("Info",True,"green")
         else:
-            infoText = smallFont.render("Info",False,"white")
+            infoText = smallFont.render("Info",True,(141,171,127))
+
+        if nameTagBox.collidepoint(mouse):
+            nameTagText = smallFont.render("NAMETAG",True,"green")
+        else:
+            nameTagText = smallFont.render("NAMETAG",True,"white")
 
 
         if quitTextBox.collidepoint(mouse):
-            quitText = smallFont.render("Quit",False,"red")
+            quitText = smallFont.render("Quit",True,"red")
         else:
-            quitText = smallFont.render("Quit",False,"white")
+            quitText = smallFont.render("Quit",True,(141,171,127))
         
 
         screen.blit(respawnButtonText,(width // 2 - 200, height // 2 - 5))
         screen.blit(pauseText,(width // 2 - 200 ,height // 2 - 150 ))
         screen.blit(infoText,(width // 2 - 200 ,height // 2 + 80 ))
         screen.blit(quitText,(width // 2 - 200,height // 2 + 160))
+        screen.blit(nameTagText,(0,0))
 
 
         clock.tick(60)
@@ -249,41 +266,70 @@ while run :
 
 
     #print(num_cells)
-
+        # NAMENSCHILDER
+        
+    screen.fill((30,32,25))
     
-    screen.fill("black")
+    def drawNames():
+        global nameTagVisible,nameTagColor
+        text = "cell"
+        text_2 = "cell_2"
+        
+        if nameTagVisible :
+            text = "cell"
+            text_2 = "cell_2"
+            nameTagColor = (255,255,255)
+        else:
+            text = ""
+            text_2 = ""
+            nameTagColor = (30,32,25)
 
-    for i in range(num_cells):
-        direction = random.choice(["RIGHT","LEFT","UP","DOWN"])
+        greenCellTextName = font.render(text,False,nameTagColor)
+        redCellTextName = font.render(text_2,False,nameTagColor)
+
+        for g_row,g_col in cells:
+            screen.blit(greenCellTextName,(g_col * cell_size - 5,g_row * cell_size - 25))
+
+        for g_row,g_col in redCells:
+            screen.blit(redCellTextName,(g_col * cell_size - 5,g_row * cell_size - 25))
+    
+    def greenCellMovement():
+        for i in range(num_cells):
+
+            direction = random.choice(["RIGHT","LEFT","UP","DOWN"])
 
         
-        col,row = cells[i]
+            col,row = cells[i]
 
-        if direction == "RIGHT" and random.random() > rightTurn :
-            if col < cols - 1:
-                col += speedGreen
-            else:
-                direction = "LEFT"
+            if direction == "RIGHT" and random.random() > rightTurn :
+                if col < cols - 1:
+                    col += speedGreen
+                else:
+                    direction = "LEFT"
 
-            #print(f"moved to the right at {col}")
-        elif direction == "LEFT" and random.random() > leftTurn :
-            if col > 0:
-                col -= speedGreen
-            else:
-                direction = "RIGHT"
+                #print(f"moved to the right at {col}")
+            elif direction == "LEFT" and random.random() > leftTurn :
+                if col > 0:
+                    col -= speedGreen
+                else:
+                    direction = "RIGHT"
 
-        elif direction == "UP" and random.random() > upTurn :
-            if row > 0:
-                row -= speedGreen
-            else:
-                direction = "DOWN"
+            elif direction == "UP" and random.random() > upTurn :
+                if row > 0:
+                    row -= speedGreen
+                else:
+                    direction = "DOWN"
 
-        elif direction == "DOWN" and random.random() > downTurn :
-            if row < rows - 1:
-                row += speedGreen
+            elif direction == "DOWN" and random.random() > downTurn :
+                if row < rows - 1:
+                    row += speedGreen
+                else:
+                    direction = "UP"
     
-        # Update die Position der aktuellen Zelle
-        cells[i] = [col,row]
+            # Update die Position der aktuellen Zelle
+            cells[i] = [col,row]
+
+    greenCellMovement()
 
     # FORTPFLANZUNG
     def fortpflanzung():
@@ -299,41 +345,43 @@ while run :
                 break
 
     fortpflanzung()
+    
+    def redCellMovement():
+        for i in range(numRedCells):
+            direction2 = random.choice(["RIGHT","LEFT","UP","DOWN"])
 
-    for i in range(numRedCells):
-        direction2 = random.choice(["RIGHT","LEFT","UP","DOWN"])
+            col2,row2 = redCells[i]
 
-        col2,row2 = redCells[i]
+            if direction2 == "RIGHT" and  0 <= col2:
+                if col2 < cols - 1:
+                    col2 += speedRed
+                else:
+                    direction = "LEFT"
 
-        if direction2 == "RIGHT" and  0 <= col2:
-            if col2 < cols - 1:
-                col2 += speedRed
-            else:
-                direction = "LEFT"
+            elif direction2 == "LEFT" and col2 > 0 and col2 < cols:
+                if col2 > 0:
+                    col2 -= speedRed
+                else:
+                    direction = "RIGHT"
 
-        elif direction2 == "LEFT" and col2 > 0 and col2 < cols:
-            if col2 > 0:
-                col2 -= speedRed
-            else:
-                direction = "RIGHT"
+            elif direction2 == "UP" and row2 > 0 and 0 <= row2:
+                if row2 > 0:
+                    row2 -= speedRed
+                else:
+                    direction = "DOWN"
 
-        elif direction2 == "UP" and row2 > 0 and 0 <= row2:
-            if row2 > 0:
-                row2 -= speedRed
-            else:
-                direction = "DOWN"
-
-        elif direction2 == "DOWN" and row2 < rows -1 and row2 < rows :
-            if row2 < rows - 1:
-                row2 += speedRed
-            else:
-                direction = "UP"
+            elif direction2 == "DOWN" and row2 < rows -1 and row2 < rows :
+                if row2 < rows - 1:
+                    row2 += speedRed
+                else:
+                    direction = "UP"
         
 
-        redCells[i] = col2,row2
+            redCells[i] = col2,row2
 
         #print(col2,row2)
-
+    
+    redCellMovement()
 
     spawnGrid(screen)
 
@@ -371,15 +419,15 @@ while run :
         blueCells[i] = col3,row3
 
 
-    
-    cellAliveText = font.render(f"Cells: {num_cells}",False,(255,255,255))
+    # Text
+    cellAliveText = normalFont.render(f"{num_cells}",True,(238,238,238))
 
     for row2,col2 in redCells:
-        redCell = pygame.draw.rect(screen,"red",(col2 * cell_size ,row2 * cell_size ,cell_size,cell_size))
+        redCell = pygame.draw.rect(screen,cellColor2,(col2 * cell_size ,row2 * cell_size ,cell_size,cell_size))
     
     
     for row,col in cells:
-        greenCell = pygame.draw.rect(screen,bodyColor,(col * cell_size ,row * cell_size ,wormSizex,wormSizey))
+        greenCell = pygame.draw.rect(screen,cellColor,(col * cell_size ,row * cell_size ,wormSizex,wormSizey))
 
         # Die gesamten Grünen Zellen prüfen
         for g_row, g_col in cells:
@@ -401,6 +449,7 @@ while run :
                 except Exception as e:
                     print(e)
 
+
     for g_row,g_col in redCells:
         if num_cells == 0:
             print("Rote Zellen sterben")
@@ -414,6 +463,7 @@ while run :
     
 
 
+    drawNames()
     #print(collsion)
     #print(num_cells)
     #print(numRedCells)
