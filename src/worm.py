e323d80a6a87c37d5d2727b7eb2e71aa3864b4ba
Moritz344 <root@moritz>
2024-12-22 14:32:20 +0100

    more in worm

--- src/worm.py
+++ src/worm.py
@@ -11,9 +11,31 @@ rows: int = 50
 cols: int = 50
 cell_size: int = 10
 
+timer = pygame.time.get_ticks()
+timerDuration = 5000
+
 # LETZE VERÄNDERUNGEN:
 # schlange braucht energie zum leben
 # schlange wird müder
+
+class Food(object):
+    def __init__(self,x,y):
+        self.foodx = x
+        self.foody = y 
+        self.food_block = 10
+
+
+    def update(self):
+        self.foodBlock = pygame.draw.rect(screen,(134, 97, 92),(self.foodx ,self.foody ,cell_size,cell_size))
+        self.foodRect = pygame.Rect(self.foodx,self.foody,cell_size,cell_size)
+        
+        for snake in snakes:
+            if self.foodBlock.colliderect(snake.snakeRect):
+                snake.energie += 500
+                snake.snake_len += 1
+                self.foodx = random.randint(0,width)
+                self.foody = random.randint(0,height)
+
 
 def spawn_grid():
     global rows,cols,x,y
@@ -25,10 +47,21 @@ def spawn_grid():
 
             #pygame.draw.rect(screen,"white",(x ,y ,cell_size ,cell_size),1)
 
+def fortpflanzung():
+    global num_snakes,timer,timerDuration
+    elapsedTime = pygame.time.get_ticks() - timer
+
+    if elapsedTime >= timerDuration :
+        timer = pygame.time.get_ticks()
+        new_snake = Snake(random.randint(0, width ),random.randint(0, height ) ,random.randint(0,2500))
+            
+        snakes.append(new_snake)
+        num_snakes += 1
+
 
 
 class Snake(object):
-    def __init__(self,x,y,energie,length=20,speed=cell_size,):
+    def __init__(self,x,y,energie,length=0,speed=cell_size,):
         global num_snakes
         self.snake_speed: int = speed
         self.snake_x = x
@@ -36,9 +69,6 @@ class Snake(object):
         self.warscheinlichkeit: float = 0#0.50
         self.energie = energie
 
-
-        self.v_timer = pygame.time.get_ticks()
-        self.v_timer_duration = 1000
 
         self.snake_len: int = length
         self.snake_list = []
@@ -48,39 +78,23 @@ class Snake(object):
         self.snake_head = [self.snake_x ,self.snake_y ]
         self.snake_list.append(self.snake_head)
 
-
-
-        
-    
-    def fortpflanzung(self):
-        global num_snakes
-        self.elapsedTime = pygame.time.get_ticks() - self.v_timer
-        
-
-        if self.elapsedTime >= self.v_timer_duration and num_snakes >= 2 and self.energie >= 150:
-            self.v_timer = pygame.time.get_ticks()
-            self.new_snake = Snake(random.randint(0, width // cell_size - 1) * cell_size,
-                                  random.randint(0, height // cell_size - 1) * cell_size,random.randint(0,50))
-            num_snakes += 1           
-            snakes.append(self.new_snake)
-
-
+        self.snakeRect = pygame.Rect(self.snake_x, self.snake_y, cell_size, cell_size)
 
     def energieHandler(self):
         # lösche eine schlange wenn sie zu wenig energie hat
         global num_snakes
-        #for i,v in enumerate(snakes):
-            #print(f"Snake {i+1}: Energie: {v.energie}")
 
         if self.energie <= 0:
-            if self in snakes:
-                num_snakes -= 1
-                snakes.remove(self)
+            num_snakes -= 1
+            snakes.remove(self)
+            self.energie = 0
         # schlange wird müder
         elif self.energie <= 100:
             self.warscheinlichkeit = 0.67
 
-        return num_snakes
+        for i,v in enumerate(snakes):
+            print(f"Snake {i+1}: Energie: {v.energie}")
+
 
     def update(self):
         global num_snakes
@@ -105,6 +119,7 @@ class Snake(object):
         if len(self.snake_list) > self.snake_len:
             del self.snake_list[0]
 
+        self.snakeRect = pygame.Rect(self.snake_x, self.snake_y, cell_size, cell_size)
         
         for snake in self.snake_list:
             self.snake = pygame.draw.rect(screen,(125,205,133),(snake[0] ,snake[1] ,cell_size,cell_size))
@@ -113,26 +128,26 @@ class Snake(object):
         self.snake_head = pygame.draw.rect(screen,"white",(self.snake_x,self.snake_y ,cell_size,cell_size))
 
         if self.snake_y < 1:
-            self.snake_y = height -200
-        elif self.snake_y > height -50:
+            self.snake_y = height 
+        elif self.snake_y > height :
             self.snake_y = 1
 
-        if self.snake_x > width - 350:
+        if self.snake_x > width :
             self.snake_x = 0
         elif self.snake_x < 1:
-            self.snake_x = width - 350
+            self.snake_x = width
 
     def get_position(self):
         return self.snake_x,self.snake_y
 
 num_snakes = 2
-snakes = [Snake(random.randint(0, 10), random.randint(0, 10), random.randint(0,1000)) for _ in range(num_snakes)]
+num_food = 100
+snakes = [Snake(random.randint(0, 900), random.randint(0, 900), random.randint(0,50000)) for _ in range(num_snakes)]
 
-for i,v in enumerate(snakes):
-    print(f"Snake {i+1}: Energie: {v.energie}")
+food = [Food(random.randint(0,width - 50),random.randint(0,height - 50)) for _ in range(num_food)]
 
-
-s = Snake(300,300,1000)
+s: object = Snake(0,0,0)
+fo: object = Food(0,0)
 
 run: bool = True
 while run:
@@ -145,12 +160,13 @@ while run:
 
     screen.fill((30,32,25))
     spawn_grid()
+    for f in food:
+        f.update()
 
+    fortpflanzung()
     for snake in snakes:
         snake.update()
-
-    s.fortpflanzung()
-    s.energieHandler()
+        snake.energieHandler()
 
     clock.tick(60)
     pygame.display.update()
