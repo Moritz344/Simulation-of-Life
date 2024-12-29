674a09bbc15486fe7c3be1f9344e53f0399e4e8a
Moritz344 <root@moritz>
2024-12-29 19:46:11 +0100

    cool particle

--- src/andere_lebewesen/particleLife.py
+++ src/andere_lebewesen/particleLife.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+# IDEA: Kollision mit einzelnen partikeln hinzufügen
 
 width = 800
 height = 600
@@ -9,28 +10,39 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("particle life")
 
 # global var
-num = 100
-colors = ["red","white","blue","yellow"]
+num = 200
+colors = ["red","white"]
 
 class Life(object):
-    def __init__(self,position,speed,velocity,color):
+    def __init__(self,position,speed,velocity,color,id):
         self.position = list(position)
         self.speed = speed
         self.velocity = velocity
         self.color = color
-        self.size = 5
+        self.size = 1
+        self.id = id
+        self.id = round(self.id,2)
+        self.direction = "RIGHT"
+
+        self.move_right_chance = 0.67
+        self.move_left_chance = 0.67
+        self.move_down_chance = 0.67
+        self.move_up_chance = 0.67
+
+
+        self.rect = pygame.Rect(self.position[0] - 10,self.position[1] - 10,self.size * 5,self.size * 5)
 
 
     def move(self):
         self.direction = random.choice(["DOWN","RIGHT","UP","LEFT"])
         
-        if self.direction == "RIGHT" and random.random() > 0.67:
+        if self.direction == "RIGHT" and random.random() > self.move_right_chance:
             self.position[0] += self.speed 
-        elif self.direction == "DOWN" and random.random() > 0.67:
+        elif self.direction == "DOWN" and random.random() > self.move_down_chance:
             self.position[1] += self.speed 
-        elif self.direction == "UP" and random.random() > 0.67:
+        elif self.direction == "UP" and random.random() > self.move_up_chance:
             self.position[1] -= self.speed
-        elif self.direction == "LEFT" and random.random() > 0.67:
+        elif self.direction == "LEFT" and random.random() > self.move_left_chance:
             self.position[0] -= self.speed
         
         if self.position[0] > width - 10:
@@ -43,17 +55,81 @@ class Life(object):
         elif self.position[1] < 0 + 10:
             self.position[1] = 0 + 10
 
+    def move_group(self,group,direction):
+        for life in group:
+            if group == white_color_lifes:
+                if direction == "RIGHT" and random.random() < self.move_right_chance:
+                    self.position[0] += self.speed * 0.05
+                elif direction == "LEFT" and random.random() < self.move_left_chance:
+                    self.position[0] -= self.speed * 0.05
+                elif direction == "UP" and random.random() < self.move_up_chance:
+                    self.position[1] -= self.speed * 0.05
+                elif direction == "DOWN" and random.random() < self.move_down_chance:
+                    self.position[1] += self.speed * 0.05
+                else:
+                    group.remove(life)
+
+        for life in group:
+            if group == red_color_lifes:
+                if direction == "RIGHT" and random.random() < self.move_right_chance:
+                    self.position[0] += self.speed * 0.1
+                elif direction == "LEFT" and random.random() < self.move_left_chance:
+                    self.position[0] -= self.speed * 0.1
+                elif direction == "UP" and random.random() < self.move_up_chance:
+                    self.position[1] -= self.speed * 0.1
+                elif direction == "DOWN" and random.random() < self.move_down_chance:
+                    self.position[1] += self.speed * 0.1
+                else:
+                    group.remove(life)
+
+    def check_collision(self):
+        global white_color_lifes,red_color_lifes
+
+        new_life = Life((random.randint(0,width),random.randint(0,height)),3,random.uniform(1,2),random.choice(colors),random.uniform(1,100))
+        red_color_lifes = [red for red in lifes if red.color == "red"]
+        white_color_lifes = [white for white in lifes if white.color == "white"]
+        groups = {0: [white_color_lifes]}
+
+        dead_lifes = []
+
+        for pixel in white_color_lifes:
+                if self.eye.colliderect(pixel.rect) :
+                    pixel.move_group(white_color_lifes,random.choice(["RIGHT","UP","LEFT","DOWN"]))
+                    
+        for pixel in red_color_lifes:
+                if pixel != self and self.eye.colliderect(pixel.rect):
+                    pixel.move_group(red_color_lifes,random.choice(["RIGHT","UP","LEFT","DOWN"]))
+                else:
+                    dead_lifes.append(pixel)
+
+
+        for all in lifes:
+            if all.position[0] > width - self.size:
+                all.position[0] = width - self.size
+            elif all.position[0] < 0 + self.size:
+                all.position[0] = self.size
+
+            if all.position[1] > height - self.size:
+                all.position[1] = height - self.size
+            elif all.position[1] < 0 + self.size:
+                all.position[1] = self.size
+
+        # remove all white ones
+        #for life in white_color_lifes:
+            #lifes.remove(life)
+
     def update(self):
-        self.move()
-        pygame.draw.circle(screen,self.color,self.position,self.size,1)
-        #pygame.draw.circle(screen,self.color,(self.position[0] - 7,self.position[1] - 7),self.size,1)
-        #pygame.draw.circle(screen,self.color,(self.position[0] + 7,self.position[1] - 7),self.size,1)
-        
+        #self.move()
+        self.life = pygame.draw.circle(screen,self.color,self.position,self.size,1)
+        #self.eye = pygame.draw.rect(screen,self.color,(self.position[0] - 20,self.position[1] - 20,50,50),1)
+        self.eye = pygame.Rect(self.position[0] - 100,self.position[1] - 50,200,100)
         self.rect = pygame.Rect(self.position[0] - 10,self.position[1] - 10,self.size * 5,self.size * 5)
-        # pygame.draw.rect(screen,"white",(self.position[0] - 10,self.position[1] - 10,self.size * 5,self.size * 5))
+        
+        #pygame.draw.rect(screen,"red",(self.position[0] - 100,self.position[1] - 50,200,200),1)
+        #pygame.draw.rect(screen,"blue",(self.position[0] - 10,self.position[1] - 10,self.size * 5,self.size * 5),1)
 
 
-lifes = [Life((random.randint(0,width),random.randint(0,height)),3,random.uniform(1,2),random.choice(colors)) for _ in range(num)]
+lifes = [Life((random.randint(0,width),random.randint(0,height)),3,random.uniform(1,2),random.choice(colors),random.uniform(1,100)) for _ in range(num)]
 
 
 run = True
@@ -69,6 +145,7 @@ while run:
 
     for life in lifes:
         life.update()
+        life.check_collision()
 
     clock.tick(60)
     pygame.display.update()
