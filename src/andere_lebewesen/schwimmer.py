2aadcac12ef2d0006bd9323ea478a4faf761c9f6
Moritz344 <root@moritz>
2025-01-03 20:56:20 +0100

    eating animation/unique schwimmer (colour)

--- src/andere_lebewesen/schwimmer.py
+++ src/andere_lebewesen/schwimmer.py
@@ -5,6 +5,7 @@ width = 800
 height = 600
 screen = pygame.display.set_mode((width,height))
 clock = pygame.time.Clock()
+fps = 60
 pygame.display.set_caption("Schwimmer Organismus")
 
 
@@ -12,7 +13,7 @@ pygame.display.set_caption("Schwimmer Organismus")
 # global var
 cell_size = 10
 num_life = 1
-
+color_choice = random.choice(["blue","red","yellow"])
 
 class Schwimmer(object):
     def __init__(self,position,colour,lebenszeit,alive,hungry,energie,speed=cell_size):
@@ -24,6 +25,7 @@ class Schwimmer(object):
         self.alive = alive
         self.hungry = hungry
         self.energie = energie
+
 
         
         # schwimmer_create variablen
@@ -42,21 +44,28 @@ class Schwimmer(object):
 
         self.block = 10
 
+        self.body_1_block = 10
+        self.body_2_block = 10
+        self.body_3_block = 10
+        self.body_4_block = 10
     
 
     def klone_schwimmer(self):
         global num_life
         elapsedTime = pygame.time.get_ticks() - self.klon_timer
-        if elapsedTime >= self.klon_timer_dur and num_life >= 1:
+        if elapsedTime >= self.klon_timer_dur and num_life >= 1 :
             self.klon_timer = pygame.time.get_ticks()
             self.klon_timer_dur += 500
             num_life += 1
             self.create_new_schwimmer()
 
+        
+
+
     def create_new_schwimmer(self):
-        if num_life >= 1:
-            new_life = Schwimmer((self.position[0],self.position[1] ),"blue",random.randint(1000,20000),True,False,random.randint(200,300))
-            schwimmer_generation.append(new_life)
+            if num_life >= 1 :
+                new_life = Schwimmer((random.randint(0,width - cell_size),random.randint(0,height - cell_size)  ),random.choice(["red","blue","green","orange",]),random.randint(0,20000),True,False,random.randint(200,300))
+                schwimmer_generation.append(new_life)
             
     def delete_schwimmer(self):
         schwimmer_generation.remove(self)
@@ -64,16 +73,28 @@ class Schwimmer(object):
     def eating_process(self):
         global num_life
         for life in schwimmer_generation:
-            if life != self and self.rect.colliderect(life.rect) and not self.alive and self in schwimmer_generation:
-                # schwimmer_generation.remove(self)
-                self.hungry = False
-                self.block -= 1
+            #if life != self and self.rect.colliderect(life.rect) and not self.alive and self in schwimmer_generation:
+            #    # schwimmer_generation.remove(self)
+            #    self.hungry = False
+            #    self.block -= 0.5 * 0.1
+            #    self.endwert -= 1 
+            #    self.startwert -= 1
+            #    break
+            if life != self and self.body_1.colliderect(life.rect) and not self.alive and self in schwimmer_generation:
+                self.body_1_block -= 1
                 
+            elif life != self and self.body_2.colliderect(life.rect) and not self.alive and self in schwimmer_generation:
+                self.body_2_block -= 1
+            elif life != self and self.body_3.colliderect(life.rect) and not self.alive and self in schwimmer_generation:
+                self.body_3_block -= 1
+            elif life != self and self.body_4.colliderect(life.rect) and not self.alive and self in schwimmer_generation:
+                self.body_4_block -= 1
     def next_generation(self):
-        global num_life 
-        if num_life == 0:
-            print("next!")
+        global num_life,next_generation
+        print("Jedes Organismus ist gestorben!")
 
+        for life in schwimmer_generation:
+            schwimmer_generation.remove(life)
         
 
     def move(self):
@@ -120,50 +141,53 @@ class Schwimmer(object):
     def schwimmer_create(self):
         self.erb = pygame.draw.rect(screen,self.colour,(self.position[0] ,self.position[1],self.block,self.block))
         self.rect = pygame.Rect(self.position[0] - cell_size * 2,self.position[1] - cell_size * 2,cell_size * 5,cell_size * 5)
-        # pygame.draw.rect( screen,"white",(self.position[0] - cell_size * 2,self.position[1] - cell_size * 2,cell_size * 5,cell_size * 5),1)
+        #pygame.draw.rect( screen,"white",(self.position[0] - cell_size * 2,self.position[1] - cell_size * 2,cell_size * 5,cell_size * 5),1)
+
 
         for i in range(self.startwert,self.endwert):
-                self.body_1 = pygame.draw.rect(screen,self.body_colour,(self.position[0] + self.abstand * i,self.position[1] - self.abstand * i,self.block,self.block))
+                self.body_1 = pygame.draw.rect(screen,self.body_colour,(self.position[0] + self.abstand * i,self.position[1] - self.abstand * i,self.body_1_block,self.body_1_block))
 
         for i in range(self.startwert,self.endwert) :
-                self.body_2 = pygame.draw.rect(screen,self.body_colour,(self.position[0] - self.abstand * i,self.position[1] - self.abstand * i,self.block,self.block))
+                self.body_2 = pygame.draw.rect(screen,self.body_colour,(self.position[0] - self.abstand * i,self.position[1] - self.abstand * i,self.body_2_block,self.body_2_block))
 
         for i in range(self.startwert,self.endwert):
-                self.body_3 = pygame.draw.rect(screen,self.body_colour,(self.position[0] - self.abstand * i,self.position[1] + self.abstand * i,self.block,self.block))
+                self.body_3 = pygame.draw.rect(screen,self.body_colour,(self.position[0] - self.abstand * i,self.position[1] + self.abstand * i,self.body_3_block,self.body_3_block))
 
         for i in range(self.startwert,self.endwert):
-                self.body_4 = pygame.draw.rect(screen,self.body_colour,(self.position[0] + self.abstand * i,self.position[1] + self.abstand * i,self.block,self.block))
+                self.body_4 = pygame.draw.rect(screen,self.body_colour,(self.position[0] + self.abstand * i,self.position[1] + self.abstand * i,self.body_4_block,self.body_4_block))
 
     def update(self):
         self.schwimmer_create()
 
-        print(num_life)
+        if num_life == 0:
+            self.next_generation()
 
-#schwimmer_family = [Schwimmer((random.randint(0,width // cell_size ),random.randint(0,height // cell_size)),) for _ in range(1)]
+
 schwimmer_generation = [Schwimmer((random.randint(0,width - cell_size * 2),random.randint(0,height - cell_size * 2)),"blue",random.randint(1000,20000),True,True,random.randint(200,300)) for _ in range(num_life)]
 
-s = Schwimmer((random.randint(0,width ),random.randint(0,height )),"blue",random.randint(1000,20000),True,True,random.randint(200,300))
+s = Schwimmer((random.randint(0,width ),random.randint(0,height )),"white",random.randint(1000,20000),True,True,random.randint(200,300))
 for i,v in enumerate(schwimmer_generation):
         print(f"{i+1},Lebensstatus: {v.alive},Lebenszeit: {v.death_timer_dur} ms, Hungrig: {v.hungry} Energie: {v.energie}")
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 run = False
-
-    screen.fill("black")
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    run = False
     
-    s.klone_schwimmer()
+        screen.fill("black")
+        
+        s.klone_schwimmer()
+        
+        for life in schwimmer_generation:
+            life.move()
+            life.update()
+            life.eating_process()
     
-    for life in schwimmer_generation:
-        life.move()
-        life.update()
-        life.eating_process()
-        life.next_generation()
-
-    clock.tick(60)
-    pygame.display.update()
+        clock.tick(fps)
+        pygame.display.update()
+main()
