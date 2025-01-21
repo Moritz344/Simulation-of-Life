7638caa9a5a10aba5a236a81b986817b4d3ed725
Moritz344 <root@moritz>
2025-01-21 18:40:42 +0100

    simple bob

--- src/andere_lebewesen/bob/main.py
+++ src/andere_lebewesen/bob/main.py
@@ -3,7 +3,6 @@ import random
 import noise
 import math
 import sys
-import sys
 
 
 width = 800
@@ -18,20 +17,29 @@ class Bob(object):
         self.size: int = size
         self.speed: int = speed
         self.time = 0
-
         self.velocity = pygame.Vector2(random.uniform(-1,1),random.uniform(-1,1))
+        self.direction = None
+
 
     def noise_bewegung(self):
-
         self.angle = (noise.pnoise1(self.time, repeat=1024) + 1)  * math.pi 
         self.dx = math.cos(self.angle) * self.speed
         self.dy = math.sin(self.angle) * self.speed 
-
+        self.angle_degress = math.atan2(self.dy,self.dx)
+        self.angle_degress = math.degrees(self.angle_degress)
+        
+        if -45 <= self.angle_degress < 45: 
+            self.direction = "Rechts"                            
+        elif 45 <= self.angle_degress < 135:                       
+                self.direction = "Unten"                             
+        elif -135 <= self.angle_degress < -45:                     
+                self.direction = "Oben"                              
+        else:           
+            self.direction = "Links"
         self.position.x += self.dx 
         self.position.y += self.dy 
         self.time += 0.01
 
-        print(self.position.x,self.position.y)
 
     def kollision_prüfen(self):
         if self.position.x >= width - self.size:
@@ -48,6 +56,11 @@ class Bob(object):
     def draw_bob(self) -> None:
         # einfacher bob zum testen
         simple_bob = pygame.draw.rect(screen,"white",(self.position.x,self.position.y,self.size,self.size))
+        pygame.draw.rect(screen,"white",(self.position.x + self.size,self.position.y + self.size,self.size,self.size))
+        pygame.draw.rect(screen,"white",(self.position.x + self.size * 2,self.position.y + self.size * 2,self.size,self.size))
+
+    def draw_tail(self,number_of_tail: int,side: str):
+        pass
     def update(self) -> None:
         self.draw_bob()
         self.noise_bewegung()
